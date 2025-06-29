ef15f23739036cdf9a534fe31c8c9a34856d3bef
Resul Aydoğmuş <39741608+Ustrif@users.noreply.github.com>
2025-06-29 16:59:31 +0000

    headers added.

--- sources/main.c
+++ sources/main.c
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beinan <beinan@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: raydogmu <raydogmu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:02:32 by raydogmu          #+#    #+#             */
-/*   Updated: 2025/04/30 21:07:08 by beinan           ###   ########.fr       */
+/*   Updated: 2025/06/29 16:58:04 by raydogmu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <readline/readline.h>
-#include <readline/history.h>
 
 t_env	*init_env(char **envp)
 {
@@ -49,7 +47,7 @@ int	ft_buildin(char **args, t_env **env_list)
 		return (ft_exit(args));
 	return (-1);
 }
-
+/*
 int	main(int ac, char **argc, char **envp)
 {
 	char	*line;
@@ -64,70 +62,16 @@ int	main(int ac, char **argc, char **envp)
 		line = readline("minishell > ");
 		if (!line)
 			break;
-		add_history(line);// BUNU KALDIRMAYI UNUTMA	
 		allwords = ft_split(line, ' ');
 		if (ft_buildin(allwords, &env_list) == -1)
 		{
 			pid = fork();
 			if (pid == 0)
-				exec(allwords[0], envp); //BURAYA DİĞER FONKSİYON GELECEK BU KISIM KALKACAK
+	exec(allwords[0], envp); //BURAYA DİĞER FONKSİYON GELECEK BU KISIM KALKACAK
 			waitpid(pid, NULL, 0);
 		}
 		free_all(allwords);
 		free(line);
 	}
 	return (0);
-}
-
-
-/*
-int	main(int ac, char **argc, char **env)
-{
-	char	*line;
-	char	**allwords;
-	int		pid;
-
-	printf("argc kullanma satiri: %s \n", argc[0]); // kullanilmiyor kaldir.
-	ac++; // ac kullanilmiyor kaldir.
-	while (1)
-	{
-		line = readline("minishell > ");
-		if (!line)
-			break;
-		add_history(line);
-		allwords = ft_split(line, ' ');
-		if(ft_buildin(allwords, env) == 0)
-		{
-			pid = fork();
-			if (pid == 0)
-				exec(allwords[0], env);
-			waitpid(-1, NULL, 0);			
-		}
-		free_all(allwords);
-		free(line);
-	}
-	return (0);
 }*/
-#include <readline/readline.h>
-#include <readline/history.h>
-
-/*
-void	free_2d_array(char **arr)
-{
-	int i = 0;
-	while (arr[i])
-		free(arr[i++]);
-	free(arr);
-}*/
-
-
-// $?+$? (127+127 gibi şeyler vermeli)
-
-/*echo "merhaba ssen"|grep x
-echo "merhaba ssen" |grep x
-echo merhaba ssen | grep x
-*/
-// git fetch --all
-// ilki komut mu?
-// tek tırnak kontrolü. tırnak chr de kalmalı.
-// 
