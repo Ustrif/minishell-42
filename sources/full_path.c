8ce2aacdaa81b670c103e66a94fb3db3f2971f27
Resul Aydoğmuş <39741608+Ustrif@users.noreply.github.com>
2025-07-02 05:51:33 +0000

    seg error fixed.

--- sources/full_path.c
+++ sources/full_path.c
@@ -6,7 +6,7 @@
 /*   By: raydogmu <raydogmu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 15:47:17 by codespace         #+#    #+#             */
-/*   Updated: 2025/06/30 12:31:45 by raydogmu         ###   ########.fr       */
+/*   Updated: 2025/07/02 05:46:50 by raydogmu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ t_list	*get_full_minis(t_token *head, char **env)
 	while (temp)
 	{
 		mini = temp->content;
-		mini->full_path = get_real_path(mini->full_cmd[0], env);
+		if (mini->full_cmd)
+			mini->full_path = get_real_path(mini->full_cmd[0], env);
 		temp->content = mini;
 		temp = temp->next;
 	}
