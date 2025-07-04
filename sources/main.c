decac3a01ab5831827e1bfa0226d8f319bfa3133
Resul Aydoğmuş <raydogmu@k1m35s01.42istanbul.com.tr>
2025-07-04 17:57:22 +0300

    main.c norm fixed.

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: raydogmu <raydogmu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 12:51:32 by raydogmu          #+#    #+#             */
-/*   Updated: 2025/07/04 17:39:07 by raydogmu         ###   ########.fr       */
+/*   Updated: 2025/07/04 17:56:46 by raydogmu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 
 int	g_status = 0;
 
-void	run(char *s, char **fenv, t_env *tenv)
+void	set_datas(int *e)
 {
-	t_promp		*prompt;
-	static int	err_code = 0;
-	char		*line;
-	struct termios saved;
-
 	if (g_status == 130)
 	{
-		err_code = 130;
+		*e = 130;
 		g_status = 0;
 	}
-    tcgetattr(STDIN_FILENO, &saved);
+}
+
+void	run(char *s, char **fenv, t_env *tenv)
+{
+	t_promp			*prompt;
+	static int		err_code = 0;
+	char			*line;
+	struct termios	saved;
+
+	set_datas(&err_code);
+	tcgetattr(STDIN_FILENO, &saved);
 	line = get_expanded_data(s, err_code);
 	if (basics(line, &err_code))
 		return ;
@@ -39,7 +44,8 @@ void	run(char *s, char **fenv, t_env *tenv)
 	prompt->tenv = tenv;
 	prompt->err_code = &err_code;
 	execute_pipeline(prompt);
-	if (g_status == 130 && (err_code == 0 || err_code == 130) && !is_builtin(((t_mini*)prompt->cmds->content)->full_cmd))
+	if (g_status == 130 && (err_code == 0 || err_code == 130)
+		&& !is_builtin(((t_mini*)prompt->cmds->content)->full_cmd))
 		write(1, "\n", 1);
 	free(line);
 	del_prompt(prompt, free_mini);
