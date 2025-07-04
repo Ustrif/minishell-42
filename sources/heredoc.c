8755f09a90c955952d6a524589ec7ffa2f20472b
Resul Aydoğmuş <raydogmu@k1m35s01.42istanbul.com.tr>
2025-07-04 18:06:54 +0300

    heredoc fixed.

--- sources/heredoc.c
+++ sources/heredoc.c
@@ -5,90 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: raydogmu <raydogmu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/24 09:20:40 by raydogmu          #+#    #+#             */
-/*   Updated: 2025/07/04 16:47:09 by raydogmu         ###   ########.fr       */
+/*   Created: 2025/07/04 18:05:47 by raydogmu          #+#    #+#             */
+/*   Updated: 2025/07/04 18:06:16 by raydogmu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void write_line(const char *s, int fd)
+void	write_line(const char *s, int fd)
 {
-    write(fd, s, ft_strlen(s));
-    write(fd, "\n", 1);
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
 }
 
-static void heredoc_child(const char *delim, int write_fd)
+static void	heredoc_child(const char *delim, int write_fd)
 {
-    char *line;
+	char	*line;
+	char	*err;
 
-    while (1)
-    {
-        line = readline("> ");
-        if (!line)
-        {
-            ft_putstr_fd("minishell: here-document delimited by end-of-file\n", 2);
-            break;
-        }
-        if (ft_strcmp(line, delim) == 0)
-        {
-            free(line);
-            break;
-        }
-        write_line(line, write_fd);
-        free(line);
-    }
-    close(write_fd);
-    exit(0);
+	err = "minishell: here-document delimited by end-of-file\n";
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			ft_putstr_fd(err, 2);
+			break ;
+		}
+		if (ft_strcmp(line, delim) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write_line(line, write_fd);
+		free(line);
+	}
+	close(write_fd);
+	exit(0);
 }
 
-static void sigint_exit(int signo)
+static void	sigint_exit(int signo)
 {
-    if (signo == SIGINT)
-    {
-        write(1, "\n", 1);
-        exit(130);
-    }
+	if (signo == SIGINT)
+	{
+		write(1, "\n", 1);
+		exit(130);
+	}
 }
 
-static void setup_heredoc_signals(void)
+static void	setup_heredoc_signals(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = sigint_exit;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags   = SA_RESETHAND;
-    sigaction(SIGINT, &sa, NULL);
+	sa.sa_handler = sigint_exit;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESETHAND;
+	sigaction(SIGINT, &sa, NULL);
 }
 
 int	open_heredoc(char *delim)
 {
-    int   fd[2];
-    pid_t pid;
-    int   status;
+	int		fd[2];
+	pid_t	pid;
+	int		status;
 
-    if (pipe(fd) == -1)
-        return -1;
-    pid = fork();
-    if (pid < 0)
-    {
-        close(fd[0]);
-        close(fd[1]);
-        return -1;
-    }
-    if (pid == 0)
-    {
-        setup_heredoc_signals();
-        close(fd[0]);
-        heredoc_child(delim, fd[1]);
-    }
-    close(fd[1]);
-    waitpid(pid, &status, 0);
-    if ((WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-        || (WIFEXITED(status) && WEXITSTATUS(status) == 130))
-    {
-        close(fd[0]);
-        return -130;
-    }
-    return fd[0];
+	if (pipe(fd) == -1)
+		return (-1);
+	pid = fork();
+	if (pid < 0)
+		return (err_case(fd[0], fd[1]));
+	if (pid == 0)
+	{
+		setup_heredoc_signals();
+		close(fd[0]);
+		heredoc_child(delim, fd[1]);
+	}
+	close(fd[1]);
+	waitpid(pid, &status, 0);
+	if ((WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
+		|| (WIFEXITED(status) && WEXITSTATUS(status) == 130))
+	{
+		close(fd[0]);
+		return (-130);
+	}
+	return (fd[0]);
 }
